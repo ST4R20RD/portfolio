efafca3c22825a246b5fcd988827a7d386d6ad43
ST4R20RD <goncalo.estrelado@hotmail.com>
2023-01-14 20:15:32 +0000

    added image to about me

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -1,7 +1,19 @@
+import { FunctionStyle } from "../FunctionStyle";
+
 export function AboutMe() {
   return (
     <div className="about-me">
       <h1>About me</h1>
+
+      <div className="flex">
+        <FunctionStyle label="<img" />
+        <img
+          src="/images/GonçaloEstrelado.jpeg"
+          alt="profile-pic"
+          className="max-h-[500px] rounded-lg"
+        />
+        <FunctionStyle label="/>" classname="mt-auto mb-0" />
+      </div>
       <p>
         I'm a software developer with a passion for learning and creating. I'm
         currently working as a full-stack developer at{" "}
@@ -11,7 +23,7 @@ export function AboutMe() {
         . I graduated at Ironhack{" "}
         <a href="https://www.linkedin.com/in/goncaloestrelado/">
           Gonçalo Estrelado
-        </a>        
+        </a>
       </p>
     </div>
   );
