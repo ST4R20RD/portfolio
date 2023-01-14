b728a687fd2431a81706f3c5e0056e515b54013f
ST4R20RD <goncalo.estrelado@hotmail.com>
2023-01-14 00:00:10 +0000

    Changed Navbar to fit in the center; Changes props of FunctionStyle comp

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Navbar, Footer, Projects, AboutMe, Presentation } from "./components";
 
 function App() {
   return (
-    <div className="bg-gray-900 h-screen">
+    <div className="bg-gray-900 h-screen w-screen">
       <Navbar />
       <Presentation />
       <Projects />
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,40 +1,35 @@
 function NavButton({ to, label, color, size }: any) {
   return (
-    <a
-      href={`#${to}`}
-      className={`font-todays text-${size}xl block py-2 ${color}`}
-    >
+    <a href={`#${to}`} className={`font-todays text-${size}xl block py-2 ${color}`}>
       {label}
     </a>
   );
 }
 
-function FunctionStyle({ children }: any) {
-  return (
-    <span className="text-gray-500 font-consolas italic px-2">{children}</span>
-  );
+function FunctionStyle({ label }: any) {
+  return <span className="text-gray-500 font-consolas italic px-2">{label}</span>;
 }
 
 export function Navbar() {
   return (
-    <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-slate-800">
-      <div className="container flex flex-wrap justify-center items-center mx-auto">
-        <FunctionStyle>const</FunctionStyle>
+    <nav className="sticky max-w-fit m-auto bg-slate-800 border-gray-200 px-2 py-2.5 rounded-full">
+      <div className="flex flex-wrap justify-center items-center">
+        <FunctionStyle label="const" />
         <a href="/">
-          <span className="self-center text-5xl font-todays font-semibold whitespace-nowrap dark:text-white">
+          <span className="self-center text-5xl font-todays font-semibold whitespace-nowrap text-white">
             GonçaloEstrelado
           </span>
         </a>
-        <FunctionStyle>=</FunctionStyle>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+        <FunctionStyle label="=" />
+        <div className="">
           <div className="flex items-center">
-            <FunctionStyle>{"(("}</FunctionStyle>
+            <FunctionStyle label="((" />
             <NavButton label="Projects" color="text-blue-400" size="4" />
-            <FunctionStyle>,</FunctionStyle>
+            <FunctionStyle label="," />
             <NavButton label="About Me" color="text-blue-400" size="4" />
-            <FunctionStyle>{") => {"}</FunctionStyle>
+            <FunctionStyle label=") => {" />
             <NavButton label="HIRE ME" color="text-purple-400" size="5" />
-            <FunctionStyle>{"})"}</FunctionStyle>
+            <FunctionStyle label="})" />
           </div>
         </div>
       </div>
