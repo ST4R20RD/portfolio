40c9c403eb04f40eed56789adfc2413162f43224
ST4R20RD <goncalo.estrelado@hotmail.com>
2023-01-15 19:33:17 +0000

    added new button to presentation comp, changed css

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -10,27 +10,31 @@ function NavButton({ to, label, color, size }: any) {
 
 export function Navbar() {
   return (
-    <nav className="sticky top-0 max-w-fit m-auto bg-slate-800 border-gray-200 px-2 py-2.5 rounded-full">
+    <nav className="sticky top-0 max-w-fit m-auto bg-secondary-white bg-opacity-5 border-gray-200 px-2 py-2.5 rounded-full">
       <div className="flex flex-wrap justify-center items-center">
         <FunctionStyle label="const" />
         <a href="/">
-          <span className="self-center text-5xl font-todays font-semibold whitespace-nowrap text-amber-100">
-            GonçaloEstrelado
+          <span className="self-center text-5xl font-todays font-semibold whitespace-nowrap text-primary-white">
+            Gonçalo_Estrelado
           </span>
         </a>
         <FunctionStyle label="=" />
         <div className="">
           <div className="flex items-center">
             <FunctionStyle label="((" />
-            <NavButton label="Projects" color="text-blue-400" size="text-4xl" />
-            <FunctionStyle label="," />
-            <NavButton label="About Me" color="text-blue-400" size="text-4xl" />
-            <FunctionStyle label=") => {" />
             <NavButton
-              label="HIRE ME"
-              color="text-purple-400"
-              size="text-5xl"
+              label="Projects"
+              color="text-secondary-white"
+              size="text-4xl"
             />
+            <FunctionStyle label="," />
+            <NavButton
+              label="About Me"
+              color="text-secondary-white"
+              size="text-4xl"
+            />
+            <FunctionStyle label=") => {" />
+            <NavButton label="HIRE ME" color="text-cyan-400" size="text-5xl" />
             <FunctionStyle label="})" />
           </div>
         </div>
--- src/components/Presentation/index.tsx
+++ src/components/Presentation/index.tsx
@@ -1,4 +1,5 @@
 import { FunctionStyle } from "../FunctionStyle";
+import { Contact } from "./../Contact/index";
 
 const SKILLS = [
   "NODEJS",
@@ -18,32 +19,47 @@ export function Presentation() {
       <div className="flex flex-col items-center">
         <div className="flex flex-col max-w-[430px]">
           <FunctionStyle label="<h1" />
-          <div className="flex font-todays text-7xl">
-            <span className="text-blue-400 mx-5">Hi, i'm</span>
-            <p className="text-amber-100">Gonçalo</p>
+          <div className="flex flex-col text-center font-todays text-7xl">
+            <span className="text-primary-gray text-lg mx-5">Hi, i'm</span>
+            <p className="text-primary-white">Gonçalo</p>
           </div>
           <FunctionStyle label="/>" classname="ml-auto mr-0" />
         </div>
         <div className="my-4">
           <FunctionStyle label="<p" />
-          <span className="text-blue-900 font-todays text-2xl">
+          <span className="text-cyan-700 font-todays text-2xl">
             Junior Web Developer
           </span>
           <FunctionStyle label="/>" />
         </div>
-        <div className="flex my-4 items-end">
-          <FunctionStyle label="let skills = [" />
-          <span className="flex">
-            {SKILLS.map((skill, index) => {
-              return (
-                <span className="flex items-end">
-                  <p className="font-todays text-cyan-400 text-3xl">{skill}</p>
-                  {index + 1 !== SKILLS.length && <FunctionStyle label="," />}
-                </span>
-              );
-            })}
-          </span>
-          <FunctionStyle label="]" />
+        <div className="flex flex-col">
+          <div className="flex my-4 items-end">
+            <FunctionStyle label="let skills = [" />
+            <span className="flex">
+              {SKILLS.map((skill, index) => {
+                return (
+                  <span className="flex items-end">
+                    <p className="font-todays text-cyan-400 text-3xl">
+                      {skill}
+                    </p>
+                    {index + 1 !== SKILLS.length && <FunctionStyle label="," />}
+                  </span>
+                );
+              })}
+            </span>
+            <FunctionStyle label="]" />
+          </div>
+          <div className="flex justify-center">
+            <button className="bg-none text-cyan-400 mx-4 p-4 border border-cyan-400 rounded-lg">
+              Download CV
+            </button>
+            <a
+              href="#"
+              className="bg-cyan-400 text-dark-blue mx-4 p-4 border border-cyan-400 rounded-lg"
+            >
+              Contact Me
+            </a>
+          </div>
         </div>
       </div>
     </div>
