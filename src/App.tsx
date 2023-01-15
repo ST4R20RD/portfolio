c8644fdac2d4a692bbe20dedee559e34f7adf4f9
ST4R20RD <goncalo.estrelado@hotmail.com>
2023-01-15 19:32:51 +0000

    changed bg color, added images to bg design

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,12 @@ import {
 
 function App() {
   return (
-    <div className="bg-gray-900 h-screen w-screen pt-8 overflow-x-hidden overflow-y-scroll scrollbar-hide">
+    <div className="bg-dark-blue bg-code-texture2 bg-no-repeat bg-[110%_center] bg-[length:800px] h-screen w-screen pt-8 overflow-x-hidden overflow-y-scroll scrollbar-hide">
+      <img
+        src="/images/code-numbers.png"
+        className="absolute left-0 top-0 h-screen opacity-60"
+        alt="code-numbers"
+      />
       <Navbar />
       <Slide>
         <Presentation />
