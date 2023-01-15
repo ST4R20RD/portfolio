383dde4fb1d310200780f6b082d102307e430773
ST4R20RD <goncalo.estrelado@hotmail.com>
2023-01-15 19:32:03 +0000

    added colors and bg images to tailwind extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,18 @@ module.exports = {
         todays: ["Todays", "sans-serif"],
         consolas: ["Consolas", "sans-serif"],
       },
+      backgroundImage: {
+        "code-texture": "url('/public/images/code-texture.png')",
+        "code-texture2": "url('/public/images/code-texture2.png')",
+        "block-texture": "url('/public/images/block-texture.png')",
+        "block-texture2": "url('/public/images/block-texture2.png')",
+      },
+      colors: {
+        "primary-white": "#F7F7F7",
+        "secondary-white": "#EEEEEE",
+        "primary-gray": "#929AAB",
+        "dark-blue": "#0A050B",
+      },
     },
   },
   plugins: [],
