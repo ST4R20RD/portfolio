e9414c514fe65547475f4b4b7cc1490c7d64a094
ST4R20RD <goncalo.estrelado@hotmail.com>
2023-01-16 19:50:28 +0000

    Redesign Css for Navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -10,44 +10,46 @@ function NavButton({ to, label, color, size }: any) {
 
 export function Navbar() {
   return (
-    <nav className="z-10 sticky top-0 max-w-fit m-auto bg-primary-gray bg-opacity-30 backdrop-blur-sm border-gray-200 px-2 py-2.5 rounded-full">
-      <div className="flex flex-wrap justify-center items-center">
-        <FunctionStyle label="const" />
-        <a href="/">
-          <span className="self-center text-5xl uppercase font-bold whitespace-nowrap text-primary-white">
-            <a
-              href="
+    <nav className="z-10 absolute top-0 w-full">
+      <div className="bg-primary-gray bg-opacity-30 backdrop-blur-sm border-gray-200 px-8 py-2.5 rounded-b-full m-auto max-w-fit">
+        <div className="flex flex-wrap justify-center items-center">
+          <FunctionStyle label="const" />
+          <a href="/">
+            <span className="self-center text-5xl uppercase font-bold whitespace-nowrap text-primary-white">
+              <a
+                href="
               #presentation"
-            >
-              Gonçalo_Estrelado
-            </a>
-          </span>
-        </a>
-        <FunctionStyle label="=" />
-        <div className="">
-          <div className="flex items-center">
-            <FunctionStyle label="((" />
-            <NavButton
-              to="#projects"
-              label="Projects"
-              color="text-secondary-white"
-              size="text-4xl"
-            />
-            <FunctionStyle label="," />
-            <NavButton
-              to="#aboutme"
-              label="About Me"
-              color="text-secondary-white"
-              size="text-4xl"
-            />
-            <FunctionStyle label=") => {" />
-            <NavButton
-              to={"mailto:goncalo.estrelado@hotmail.com"}
-              label="HIRE ME"
-              color="text-cyan-400"
-              size="text-5xl"
-            />
-            <FunctionStyle label="})" />
+              >
+                Gonçalo_Estrelado
+              </a>
+            </span>
+          </a>
+          <FunctionStyle label="=" />
+          <div className="">
+            <div className="flex items-center">
+              <FunctionStyle label="((" />
+              <NavButton
+                to="#projects"
+                label="Projects"
+                color="text-secondary-white"
+                size="text-4xl"
+              />
+              <FunctionStyle label="," />
+              <NavButton
+                to="#aboutme"
+                label="About Me"
+                color="text-secondary-white"
+                size="text-4xl"
+              />
+              <FunctionStyle label=") => {" />
+              <NavButton
+                to={"mailto:goncalo.estrelado@hotmail.com"}
+                label="HIRE ME"
+                color="text-cyan-400"
+                size="text-5xl"
+              />
+              <FunctionStyle label="})" />
+            </div>
           </div>
         </div>
       </div>
