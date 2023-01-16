852d18e05d4fe59ead8358183bc5dda4793bf64b
ST4R20RD <goncalo.estrelado@hotmail.com>
2023-01-16 01:02:38 +0000

    added links to card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -7,9 +7,11 @@ interface CardProps {
   subTitle: string;
   description: string;
   techs: Array<string>;
+  github: string;
+  live: string;
 }
 
-export function Card({ image, name, subTitle, description, techs }: CardProps) {
+export function Card({ image, name, subTitle, description, techs, github, live }: CardProps) {
   return (
     <div className="bg-[#0C0F16] p-4 m-5 border-t border-cyan-400 flex justify-center w-[800px]">
       <img src={image} alt="projectImg" className="h-[300px]" />
@@ -17,14 +19,18 @@ export function Card({ image, name, subTitle, description, techs }: CardProps) {
         <p className="text-3xl">{name}</p>
         <p className="text-xl py-2">{subTitle}</p>
         <p className="max-w-prose py-2">{description}</p>
-        <p className="flex">
+        <p className="flex justify-between">
           {techs.map((tech) => {
             return <p className="text-zinc-600 pr-2">{tech}</p>;
           })}
         </p>
         <div className="flex justify-end text-3xl my-2">
-          <TbBrandGithub className="mx-4" />
-          <MdOpenInNew className="mx-4" />
+          <a href={github} target="_blank" rel="noreferrer">
+            <TbBrandGithub className="mx-4" />
+          </a>
+          <a href={live} target="_blank" rel="noreferrer">
+            <MdOpenInNew className="mx-4" />
+          </a>
         </div>
       </div>
     </div>
