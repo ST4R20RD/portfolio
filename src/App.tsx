396e8ea4a109ab5ba3f336046a2797250d8df13c
ST4R20RD <goncalo.estrelado@hotmail.com>
2023-01-16 02:23:16 +0000

    fix bg

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 import "./App.css";
-import {
-  Navbar,
-  Footer,
-  Projects,
-  AboutMe,
-  Presentation,
-  Slide,
-} from "./components";
+import { Navbar, Footer, Projects, AboutMe, Presentation, Slide } from "./components";
 
 function App() {
   return (
     <div className="bg-dark-blue bg-code-texture2 bg-no-repeat bg-[110%_center] bg-[length:800px] h-screen w-screen pt-8 overflow-x-hidden overflow-y-scroll scrollbar-hide">
       <img
         src="/images/code-numbers.png"
-        className="absolute left-0 top-0 h-screen opacity-60"
+        className="z-10 absolute left-0 top-0 h-screen"
         alt="code-numbers"
       />
       <Navbar />
