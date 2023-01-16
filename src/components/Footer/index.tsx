c8a8ca0f1cfe00047dd627911dcef5f06145a297
ST4R20RD <goncalo.estrelado@hotmail.com>
2023-01-16 02:29:21 +0000

    added links to footer and fixed width

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,23 +1,25 @@
 export function Footer() {
   return (
-    <footer className="left-0 bottom-0 right-0 p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
-      <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-        © {new Date().getFullYear()} -{" "}
-        <a
-          href="
-                https://www.linkedin.com/in/goncaloestrelado/
-                "
-        >
-          Gonçalo Estrelado
-        </a>
-      </span>
-      <div className="flex m-3 text-sm text-gray-500">
-        <a href="#" className="px-2">
-          About me
-        </a>
-        <a href="#" className="px-2">
-          HIRE ME
-        </a>
+    <footer className="ml-14">
+      <div className="flex items-center justify-between p-4 bg-primary-gray bg-opacity-30 backdrop-blur-sm">
+        <span className="text-sm text-gray-500 sm:text-center">
+          © {new Date().getFullYear()} -{" "}
+          <a
+            href="
+          https://www.linkedin.com/in/goncaloestrelado/
+          "
+          >
+            Gonçalo Estrelado
+          </a>
+        </span>
+        <span className="w-1/5 flex justify-around text-sm font-semibold text-gray-500">
+          <a href="#projects">Projects</a>
+          <a href="#aboutme">About me</a>
+        </span>
+        <div className="flex m-3 text-sm text-gray-500">
+          <p className="px-2">Send me an email:</p>
+          <button className="px-2 text-cyan-400 font-bold">Contact</button>
+        </div>
       </div>
     </footer>
   );
